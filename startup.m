526a007525b57a31ada5aba7dcbe510ad0f0b6b2
Jason Sherfey <jssherfey@gmail.com>
2014-01-31 06:53:27 -0500

    added matlab startup.m (eg,to generate and set path for netmodeler)

---
+++ startup.m
@@ -0,0 +1,5 @@
+addpath(genpath('/usr3/graduate/sherfey/research/modeling'));
+addpath /usr3/graduate/sherfey/research/analysis/mmil/matlab/matext % eg. splitstr()
+addpath /usr3/graduate/sherfey/research/analysis/mmil/matlab/mmil_util % eg. mmil_args2parms()
+addpath /usr3/graduate/sherfey/research/analysis/mmil/matlab/fieldtrip-20080624_private % eg. issubfield()
+
